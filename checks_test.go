df5e41e091171c2c5a88bf9d4270f51419de2867
Romain Libaert <git@rlibaert.dev>
2025-06-20 23:00:07 +0200

    feat: rewrote tests for Checks.Run

--- checks_test.go
+++ checks_test.go
@@ -2,33 +2,49 @@ package genki_test
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"time"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/oaiiae/genki"
 )
 
-func ExampleChecks_Run() {
-	fmt.Println(genki.Checks{
-		"a": func(context.Context) error { return nil },
-		"b": func(context.Context) error { return io.EOF },
-	}.Run(context.Background()))
+func TestChecks_Run(t *testing.T) {
+	anError := errors.New("an error")
 
-	//Output:
-	// [b] EOF
-}
-
-func ExampleAlways() {
-	fmt.Println(genki.Always().Run(context.Background()))
-
-	//Output:
-	// <nil>
-}
-
-func ExampleAfter() {
-	fmt.Println(genki.After(time.Hour).Run(context.Background()))
-
-	//Output:
-	// [after] xx left
+	testCases := []struct {
+		desc      string
+		checks    genki.Checks
+		targetErr error
+	}{
+		{
+			desc: "nil",
+			checks: genki.Checks{
+				"a": func(context.Context) error { return nil },
+			},
+			targetErr: nil,
+		},
+		{
+			desc: "anError",
+			checks: genki.Checks{
+				"b": func(context.Context) error { return anError },
+			},
+			targetErr: anError,
+		},
+		{
+			desc: "nil and anError",
+			checks: genki.Checks{
+				"a": func(context.Context) error { return nil },
+				"b": func(context.Context) error { return anError },
+			},
+			targetErr: anError,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			err := tC.checks.Run(context.Background())
+			assert.ErrorIs(t, err, tC.targetErr)
+		})
+	}
 }
